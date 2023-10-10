f312420b1429333f840307e3c67a10e6d7222f59
Rui Nascimento <sruy19@gmail.com>
2023-10-10 09:17:10 -0300

    chore: script with table users

---
+++ src/main/resources/db.migration/V1__createTableUsers.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username   VARCHAR(30) UNIQUE NOT NULL,
+    name       VARCHAR(100)        NOT NULL,
+    password   VARCHAR(255)  NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
